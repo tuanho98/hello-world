125c3196a24f762031eddeebc538ec79cf5441e5
yourGitHubusername <hominhtuan1998@gmail.com>
2020-04-16 16:22:15 -0400

    initial commit

---
+++ src/day01/basic.java
@@ -0,0 +1,16 @@
+package day01;
+
+import java.util.Scanner;
+
+public class basic {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		Scanner in =new Scanner(System.in);
+		String s=in.nextLine();
+		
+		System.out.println(s);
+
+	}
+
+}
